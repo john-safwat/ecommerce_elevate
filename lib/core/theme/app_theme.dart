555577871a3d0b63ccf8ec3602acccfb23dde8e4
John Safwat <106314101+john-safwat@users.noreply.github.com>
2024-11-14 23:32:04 +0200

    feat(Home) - ECOM-8 : Create Home Screen With Button Navigation Bar - create Home Screen - create bottom nav bar with 4 items - add bottom nav bar style to app theme data - make the tab change on bottom nav bar select

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -114,5 +114,14 @@ class AppTheme {
       side: BorderSide(color: AppColors.pink),
       overlayColor: WidgetStatePropertyAll(AppColors.pink),
     ),
+
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: AppColors.white,
+      selectedItemColor: AppColors.pink,
+      unselectedItemColor: AppColors.white[AppColors.colorCode80],
+      showUnselectedLabels: true,
+      showSelectedLabels: true
+    )
   );
 }
