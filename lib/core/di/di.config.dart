06a32104817a4117fabc9c80d6692fe6363661ad
John Safwat <106314101+john-safwat@users.noreply.github.com>
2024-11-21 14:24:15 +0200

    update retrofit client and model

---
+++ lib/core/di/di.config.dart
@@ -0,0 +1,195 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:dio/dio.dart' as _i361;
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
+import 'package:logger/logger.dart' as _i974;
+import 'package:pretty_dio_logger/pretty_dio_logger.dart' as _i528;
+import 'package:shared_preferences/shared_preferences.dart' as _i460;
+
+import '../../features/auth/data/api/auth_retrofit_client.dart' as _i187;
+import '../../features/auth/data/datasource/contract/auth_local_datasource.dart'
+    as _i1071;
+import '../../features/auth/data/datasource/contract/auth_remote_datasource.dart'
+    as _i1070;
+import '../../features/auth/data/datasource/impl/auth_local_datasource_impl.dart'
+    as _i909;
+import '../../features/auth/data/datasource/impl/auth_remote_datasource_impl.dart'
+    as _i81;
+import '../../features/auth/data/repository/auth_repository_impl.dart' as _i409;
+import '../../features/auth/domain/repository/auth_repository.dart' as _i961;
+import '../../features/auth/domain/use_case/delete_token_use_case.dart'
+    as _i376;
+import '../../features/auth/domain/use_case/forget_password_use_case.dart'
+    as _i90;
+import '../../features/auth/domain/use_case/login_user_use_case.dart' as _i697;
+import '../../features/auth/domain/use_case/reset_password_use_case.dart'
+    as _i149;
+import '../../features/auth/domain/use_case/signup_user_use_case.dart' as _i529;
+import '../../features/auth/domain/use_case/verify_reset_code_use_case.dart'
+    as _i660;
+import '../../features/auth/presentation/forget_password/forget_password_view_model.dart'
+    as _i599;
+import '../../features/auth/presentation/login/login_view_model.dart' as _i225;
+import '../../features/auth/presentation/otp_verify/otp_verify_view_model.dart'
+    as _i1033;
+import '../../features/auth/presentation/reset_password/reset_password_view_model.dart'
+    as _i974;
+import '../../features/auth/presentation/signup/signup_view_model.dart'
+    as _i1055;
+import '../../features/home/data/api/best_seller/products_retrofit_client.dart'
+    as _i337;
+import '../../features/home/data/api/categories/categories_retrofit_client.dart'
+    as _i186;
+import '../../features/home/data/api/occasions/occasions_retrofit_client.dart'
+    as _i207;
+import '../../features/home/data/datasource/contract/best_seller_datasource.dart'
+    as _i728;
+import '../../features/home/data/datasource/contract/categories_datasource.dart'
+    as _i32;
+import '../../features/home/data/datasource/contract/occasions_datasource.dart'
+    as _i28;
+import '../../features/home/data/datasource/impl/best_seller_datasource_impl.dart'
+    as _i552;
+import '../../features/home/data/datasource/impl/categories_datasource_impl.dart'
+    as _i282;
+import '../../features/home/data/datasource/impl/occasions_datasource_impl.dart'
+    as _i291;
+import '../../features/home/data/repository/best_seller_repository_impl.dart'
+    as _i830;
+import '../../features/home/data/repository/categories_repository_impl.dart'
+    as _i199;
+import '../../features/home/data/repository/occasions_repository_impl.dart'
+    as _i17;
+import '../../features/home/domain/repository/best_seller_repository.dart'
+    as _i210;
+import '../../features/home/domain/repository/categories_repository.dart'
+    as _i403;
+import '../../features/home/domain/repository/occasions_repository.dart'
+    as _i860;
+import '../../features/home/domain/use_case/occasions_use_case.dart' as _i896;
+import '../../features/home/tabs/home/view_model/home_tab_view_model.dart'
+    as _i183;
+import '../../features/home/view_model/home_view_model.dart' as _i656;
+import '../datasource_execution/datasource_execution.dart' as _i166;
+import '../modules/logger_module.dart' as _i774;
+import '../modules/network_module.dart' as _i184;
+import '../modules/shared_preferences_module.dart' as _i744;
+import '../providers/app_config_provider.dart' as _i56;
+import '../providers/language_provider.dart' as _i822;
+import '../utils/app_initializer.dart' as _i348;
+
+extension GetItInjectableX on _i174.GetIt {
+// initializes the registration of main-scope dependencies inside of GetIt
+  Future<_i174.GetIt> init({
+    String? environment,
+    _i526.EnvironmentFilter? environmentFilter,
+  }) async {
+    final gh = _i526.GetItHelper(
+      this,
+      environment,
+      environmentFilter,
+    );
+    final sharedPreferencesModule = _$SharedPreferencesModule();
+    final loggerModule = _$LoggerModule();
+    final networkModule = _$NetworkModule();
+    await gh.factoryAsync<_i460.SharedPreferences>(
+      () => sharedPreferencesModule.prefs,
+      preResolve: true,
+    );
+    gh.factory<_i656.HomeViewModel>(() => _i656.HomeViewModel());
+    gh.singleton<_i166.DataSourceExecution>(() => _i166.DataSourceExecution());
+    gh.singleton<_i56.AppConfigProvider>(() => _i56.AppConfigProvider());
+    gh.lazySingleton<_i974.Logger>(() => loggerModule.loggerProvider);
+    gh.lazySingleton<_i974.PrettyPrinter>(() => loggerModule.prettyPrinter);
+    gh.lazySingleton<_i361.Dio>(() => networkModule.provideDio());
+    gh.lazySingleton<_i528.PrettyDioLogger>(
+        () => networkModule.providerInterceptor());
+    gh.factory<_i728.BestSellerDatasource>(() => _i552.BestSellerDatasourceImpl(
+          gh<InvalidType>(),
+          gh<_i166.DataSourceExecution>(),
+        ));
+    gh.singleton<_i187.AuthRetrofitClient>(
+        () => _i187.AuthRetrofitClient(gh<_i361.Dio>()));
+    gh.singleton<_i186.CategoriesRetrofitClient>(
+        () => _i186.CategoriesRetrofitClient(gh<_i361.Dio>()));
+    gh.singleton<_i207.OccasionsRetrofitClient>(
+        () => _i207.OccasionsRetrofitClient(gh<_i361.Dio>()));
+    gh.singleton<_i337.ProductsRetrofitClient>(
+        () => _i337.ProductsRetrofitClient(gh<_i361.Dio>()));
+    gh.factory<_i210.BestSellerRepository>(
+        () => _i830.BestSellerRepositoryImpl(gh<_i728.BestSellerDatasource>()));
+    gh.factory<_i1071.AuthLocalDatasource>(
+        () => _i909.AuthLocalDatasourceImpl(gh<_i460.SharedPreferences>()));
+    gh.singleton<_i822.LanguageProvider>(
+        () => _i822.LanguageProvider(gh<_i460.SharedPreferences>()));
+    gh.factory<_i32.CategoriesDatasource>(() => _i282.CategoriesDatasourceImpl(
+          gh<_i186.CategoriesRetrofitClient>(),
+          gh<_i166.DataSourceExecution>(),
+        ));
+    gh.factory<_i28.OccasionsDatasource>(() => _i291.OccasionsDatasourceImpl(
+          gh<_i207.OccasionsRetrofitClient>(),
+          gh<_i166.DataSourceExecution>(),
+        ));
+    gh.factory<_i403.CategoriesRepository>(
+        () => _i199.CategoriesRepositoryImpl(gh<_i32.CategoriesDatasource>()));
+    gh.factory<_i860.OccasionsRepository>(
+        () => _i17.OccasionsRepositoryImpl(gh<_i28.OccasionsDatasource>()));
+    gh.factory<_i1070.AuthRemoteDatasource>(() => _i81.AuthRemoteDatasourceImpl(
+          gh<_i187.AuthRetrofitClient>(),
+          gh<_i166.DataSourceExecution>(),
+        ));
+    gh.lazySingleton<_i348.AppInitializer>(() => _i348.AppInitializer(
+          gh<_i822.LanguageProvider>(),
+          gh<_i56.AppConfigProvider>(),
+          gh<_i460.SharedPreferences>(),
+        ));
+    gh.factory<_i961.AuthRepository>(() => _i409.AuthRepositoryImpl(
+          gh<_i1070.AuthRemoteDatasource>(),
+          gh<_i1071.AuthLocalDatasource>(),
+        ));
+    gh.factory<_i896.OccasionsUseCase>(
+        () => _i896.OccasionsUseCase(gh<_i860.OccasionsRepository>()));
+    gh.factory<_i90.ForgetPasswordUseCase>(
+        () => _i90.ForgetPasswordUseCase(gh<_i961.AuthRepository>()));
+    gh.factory<_i529.SignupUserUseCase>(
+        () => _i529.SignupUserUseCase(gh<_i961.AuthRepository>()));
+    gh.factory<_i376.DeleteTokenUseCase>(
+        () => _i376.DeleteTokenUseCase(gh<_i961.AuthRepository>()));
+    gh.factory<_i1055.SignupViewModel>(
+        () => _i1055.SignupViewModel(gh<_i529.SignupUserUseCase>()));
+    gh.factory<_i183.HomeTabViewModel>(
+        () => _i183.HomeTabViewModel(gh<_i896.OccasionsUseCase>()));
+    gh.factory<_i697.LoginUserUseCase>(
+        () => _i697.LoginUserUseCase(gh<_i961.AuthRepository>()));
+    gh.factory<_i149.ResetPasswordUseCase>(
+        () => _i149.ResetPasswordUseCase(gh<_i961.AuthRepository>()));
+    gh.factory<_i660.VerifyResetPasswordUseCase>(
+        () => _i660.VerifyResetPasswordUseCase(gh<_i961.AuthRepository>()));
+    gh.factory<_i974.ResetPasswordViewModel>(
+        () => _i974.ResetPasswordViewModel(gh<_i149.ResetPasswordUseCase>()));
+    gh.factory<_i599.ForgetPasswordViewModel>(
+        () => _i599.ForgetPasswordViewModel(gh<_i90.ForgetPasswordUseCase>()));
+    gh.factory<_i225.LoginViewModel>(
+        () => _i225.LoginViewModel(gh<_i697.LoginUserUseCase>()));
+    gh.factory<_i1033.OtpVerifyViewModel>(() => _i1033.OtpVerifyViewModel(
+          gh<_i660.VerifyResetPasswordUseCase>(),
+          gh<_i90.ForgetPasswordUseCase>(),
+        ));
+    return this;
+  }
+}
+
+class _$SharedPreferencesModule extends _i744.SharedPreferencesModule {}
+
+class _$LoggerModule extends _i774.LoggerModule {}
+
+class _$NetworkModule extends _i184.NetworkModule {}
---
+++ lib/features/home/data/models/products/product_dto.g.dart
@@ -0,0 +1,45 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'product_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ProductDtp _$ProductDtpFromJson(Map<String, dynamic> json) => ProductDtp(
+      productId: json['_id'] as String?,
+      title: json['title'] as String?,
+      slug: json['slug'] as String?,
+      description: json['description'] as String?,
+      imgCover: json['imgCover'] as String?,
+      images:
+          (json['images'] as List<dynamic>?)?.map((e) => e as String).toList(),
+      price: (json['price'] as num?)?.toInt(),
+      priceAfterDiscount: (json['priceAfterDiscount'] as num?)?.toInt(),
+      quantity: (json['quantity'] as num?)?.toInt(),
+      category: json['category'] as String?,
+      occasion: json['occasion'] as String?,
+      createdAt: json['createdAt'] as String?,
+      updatedAt: json['updatedAt'] as String?,
+      V: (json['__v'] as num?)?.toInt(),
+      id: json['id'] as String?,
+    );
+
+Map<String, dynamic> _$ProductDtpToJson(ProductDtp instance) =>
+    <String, dynamic>{
+      '_id': instance.productId,
+      'title': instance.title,
+      'slug': instance.slug,
+      'description': instance.description,
+      'imgCover': instance.imgCover,
+      'images': instance.images,
+      'price': instance.price,
+      'priceAfterDiscount': instance.priceAfterDiscount,
+      'quantity': instance.quantity,
+      'category': instance.category,
+      'occasion': instance.occasion,
+      'createdAt': instance.createdAt,
+      'updatedAt': instance.updatedAt,
+      '__v': instance.V,
+      'id': instance.id,
+    };
